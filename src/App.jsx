ad58f450d234cca0760df25a2831c768158ebcef
huyhq101096@gmail.com <huyhq101096@gmail.com>
2023-05-29 22:33:34 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import './index.css'
+import Header from './component/header/Header'
+import Footer from './component/footer/Footer'
+import Nav from './component/nav/Nav'
+
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <Footer />
+      <Nav />
+    
+    </>
+  )
+
+
 };
 
 
