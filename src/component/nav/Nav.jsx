a1bf517199fd645e68c620524a088c6f9f879b60
Huy <huy.huynh@vsss.edu.vn>
2023-05-30 17:05:07 +0700

    update

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import './nav.css'
+import React from "react";
+import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BiBook } from "react-icons/bi";
+import { RiServiceLine } from "react-icons/ri";
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { useState } from "react";
 
 const Nav = () => {
-  return (
-    <div>Nav</div>
-  )
-}
+  const [activeNav, setActiveNav] = useState("#");
 
-export default Nav
+  return (
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a href="#experience">
+        <BiBook />
+      </a>
+      <a href="#service">
+        <RiServiceLine />
+      </a>
+      <a href="#contact">
+        <BiMessageSquareDetail />
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
